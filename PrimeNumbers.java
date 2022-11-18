e73973429893d1e14d74cdd909264f73c7f1083f
Portapila <portapila@gmail.com>
2022-11-18 16:33:56 -0300

    justa backup

--- PrimeNumbers.java
+++ PrimeNumbers.java
@@ -1,58 +1,59 @@
 import java.util.Scanner;
 
 /*
+ * teste de matriz
  * incluir classes para:
  * - debug
   */
 
 public class PrimeNumbers extends Object {
-    public static void main (String[] args){
-        final int MAX_NUMBER = 10000000;
-        boolean isNatural[];
-        int naturalNumber = -1;
-        Scanner input = new Scanner(System.in);
+	public static void main(String[] args) {
+		final int MAX_NUMBER = 100000001;
+		boolean isNatural[];
+		int naturalNumber = -1;
+		Scanner input = new Scanner(System.in);
 
-        // get a number from user
+		// get a number from user
 
-        while (naturalNumber < 1 || naturalNumber > MAX_NUMBER)
-        {
-            System.out.print("Type a Natural Number from 1 to " + MAX_NUMBER + ": ");
-            String typedNumber = input.next();
-            System.out.println("You typed " + typedNumber);
-            naturalNumber = Integer.parseInt(typedNumber);
-    
-        }
+		while (naturalNumber < 1 || naturalNumber > MAX_NUMBER) {
+			System.out.print("Type a Natural Number from 1 to " + MAX_NUMBER + ": ");
+			String typedNumber = input.next();
+			System.out.println("You typed " + typedNumber);
 
-        // allocate memory
-        isNatural = new boolean[naturalNumber + 1];
+			if (typedNumber.toUpperCase().equals("MAX")) {
 
-        // mark all not prime numbers
-        int i, j;
+				naturalNumber = MAX_NUMBER;
+			} else {
+				naturalNumber = Integer.parseInt(typedNumber);
+			}
 
-        for (i = 2; i <= naturalNumber; i++)
-        {
-            //System.out.println("checking " + i + "-----------");
-            if (!isNatural[i])
-            {
-                for (j = 2; (i*j) <= naturalNumber; j++)
-                {
-                    isNatural[i*j] = true;
-                    //System.out.println("Setting " + (i*j) + " as natural");
-                }
-            }
-        }
+		}
 
-        // print prime numbers list
-        System.out.println("Prime Numbers List");
-        j = 0;
-        for (i=2; i <= naturalNumber; i++)
-        {
-            if (!isNatural[i])
-            {
-                j++;
-                System.out.print("(" + j + ")" + i + ",");
-            }
-        }
-        System.out.println("\n---------------\nCount:" + j);
-    }    
+		// allocate memory
+		isNatural = new boolean[naturalNumber + 1];
+
+		// mark all not prime numbers
+		int i, j;
+
+		for (i = 2; i <= naturalNumber; i++) {
+			// System.out.println("checking " + i + "-----------");
+			if (!isNatural[i]) {
+				for (j = 2; (i * j) <= naturalNumber; j++) {
+					isNatural[i * j] = true;
+					// System.out.println("Setting " + (i*j) + " as natural");
+				}
+			}
+		}
+
+		// print prime numbers list
+		System.out.println("Prime Numbers List");
+		j = 0;
+		for (i = 2; i <= naturalNumber; i++) {
+			if (!isNatural[i]) {
+				j++;
+				System.out.printf("(%d);%d\n", j, i);
+			}
+		}
+		System.out.println("\n---------------\nCount:" + j);
+	}
 }
---
+++ StringsTesting.java
@@ -0,0 +1,32 @@
+
+public class StringsTesting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String MyStr = "Alex Portapila The Writer   ";
+		
+		System.out.println("raw[" + MyStr + "]\n");
+		System.out.println("toLowerCase()=[" + MyStr.toLowerCase() + "]");
+		System.out.println("toUpperCase()=[" + MyStr.toUpperCase() + "]");
+		System.out.println("toLowerCase().trim()=[" + MyStr.toLowerCase().trim() + "]");
+		System.out.println("substring(5)=[" + MyStr.substring(5) + "]");
+		System.out.println("substring(5,14)=[" + MyStr.substring(5,14) + "]");
+		System.out.println("replace('a','x')[" + MyStr.replace('a','x') + "]");
+		System.out.println("replace(\"Alex\",\"Clebe\")[" + MyStr.replace("Alex","Cleber") + "]");
+		System.out.println("indexOf(\"Portapila\")=[" + MyStr.indexOf("Portapila") + "]");
+		System.out.println("lastIndexOf(\"Portapila\")=[" + MyStr.lastIndexOf("Portapila") + "]");
+		
+		String vector[] = MyStr.split(" ");
+		int i;
+		System.out.print("split(\' \')=");
+		for (i=0; vector[i] != null; i++)
+		{
+			System.out.print("[" + vector[i] + "]");
+		}
+		System.out.println("");
+
+	}
+
+}
+
